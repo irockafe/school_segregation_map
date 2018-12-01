0ca7205d83ccd83a390d4482d922bacd6c827d2d
root <root@school-seg-template-6.us-east1-b.c.school-segregation.internal>
2018-12-01 20:45:51 +0000

    updated some dependencies

--- dodo.py
+++ dodo.py
@@ -1,5 +1,6 @@
 import pathlib
 import logging
+import doit.tools
 
 logging.basicConfig(level=logging.DEBUG, 
         handlers=[logging.FileHandler("doit.log", mode='w', delay=False),
@@ -13,10 +14,12 @@ DOIT_CONFIG = {'check_file_uptodate': 'timestamp',
 # Assume baseline path is where dodo.py is located
 PATH = pathlib.Path().absolute() 
 # Number of miles around which to search for neighboring schools
-RADII = [1, 2.5, 5, 7.5, 15, 20]
+RADII = [1, 2.5, 5, 7.5, 10, 15, 20]
 DATA_PATH = PATH / 'data'
 CODE_PATH = PATH / 'code'
 LOG_PATH = PATH / 'logs'
+NEIGHBOR_DATA_FNAME = 'racial_composition_shared_grades_dict_%.1f_miles.pkl' 
+
 for path in [DATA_PATH, CODE_PATH, LOG_PATH]: 
     if not path.exists():
         path.mkdir()
@@ -47,26 +50,50 @@ def task_organize_data():
 
 def task_regional_breakdown():
     '''
-    Get a xarray dataset, where each variable is a school, dims = school x race, 
-    and coordinates are school_id and racial group
+    Get a nested dict of dataframes for race and lunch data, 
+    Shape: (school_id, racial_groups), and (school_id, lunch_info)
+    This takes about 6 hours, so run-once is enabled
     '''
     # Using floats to match with process_data, which names files with floats
     for radius in RADII:
         radius = float(radius)
         yield {
-            'targets':[DATA_PATH / ('racial_composition_shared_grades_%s_miles.pkl' % radius),
-                       DATA_PATH / ('racial_composition_shared_grades%s_miles.pkl' % radius),
-                       DATA_PATH / ('lunch_data_shared_grades_%s_miles.pkl' % radius),
-                       DATA_PATH / ('lunch_data_all_neighbors_%s_miles.pkl' % radius)
+            'targets':[DATA_PATH / (NEIGHBOR_DATA_FNAME % radius)
                        ],
             'file_dep':[CODE_PATH / 'regional_racial_breakdown.py',
                         'data/organized_data.pkl',
                         'data/school_distances_kdTree.pkl'
                         ],
             'actions':['python code/regional_racial_breakdown.py --radius {rad}'.format(rad=radius)],
-            'name': 'regional_racial_breakdown_{radius}'.format(radius=radius)
+            'name': 'regional_racial_breakdown_{radius}'.format(radius=radius),
+            'uptodate': [doit.tools.run_once] 
         }
 
+
+def task_racial_percent_differences():
+    '''
+    Get the difference in percentages between all neighboring schools
+    '''
+    for radius in RADII:
+        radius = float(radius)
+        yield {
+            'targets': [
+                DATA_PATH / ('race_counts_schools_%s.pkl' % radius), 
+                DATA_PATH / ('race_counts_neighbors_%s.pkl' % radius), 
+                DATA_PATH / ('lunch_counts_schools_%s.pkl' % radius), 
+                DATA_PATH / ('lunch_counts_neighbors_%s.pkl' % radius), 
+                DATA_PATH / ('lunch_percent_diff_shared_grades_%s.pkl' % radius), 
+                DATA_PATH / ('racial_percent_diff_shared_grades_%s.pkl' % radius), 
+                ],
+            'file_dep': [DATA_PATH / (NEIGHBOR_DATA_FNAME % radius),
+                CODE_PATH / 'percentage_diffs.py'],
+            'actions': ['python {script} -f {data} -r {radius}'.format(
+                script=(CODE_PATH / 'percentage_diffs.py'), 
+                data=(DATA_PATH / (NEIGHBOR_DATA_FNAME % radius)), 
+                radius=radius)],
+            'name': 'racial_percent_differences_{rad}'.format(
+                rad=radius)
+            }
 
 def task_analyze_data():
     # do some of the analysis of the data
